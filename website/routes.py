13c4ae4421f198792d884415430adccc16e97f1d
Wilest <2954067708@qq.com>
2021-04-11 18:34:20 +0800

    update

--- website/routes.py
+++ website/routes.py
@@ -1,13 +1,18 @@
 from flask import Flask, render_template, url_for, request, redirect, flash, session
 from website import app, db
+from website.forms import IndexForm
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index',methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    form = IndexForm()
+    if (form.validate_on_submit()):
+        redirect(url_for("result"))
+    return render_template('index.html',form=form)
 
-@app.route('/result')
+
+@app.route('/result',methods=['GET','POST'])
 def result():
     return render_template('result.html')
 
