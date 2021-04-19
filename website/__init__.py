d332c69120b2979247c0ed832f8810da30904cc2
HecSky <hecsky@163.com>
2021-04-19 09:11:35 +0800

    decision tree

--- website/__init__.py
+++ website/__init__.py
@@ -3,11 +3,6 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-# app = Flask(__name__, static_folder="static")
-# app.config['SECRET_KEY'] = os.urandom(24)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c2097255:Database2021@csmysql.cs.cf.ac.uk:3306/c2097255_informatics'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 app = Flask(__name__, static_folder="static")
 app.config['SECRET_KEY'] = 'f8ee4cbd64785576bab7b14bc37747640c6a4837cf6cd0c5'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://c2096406:EUAQCKCXfeng1357@csmysql.cs.cf.ac.uk:3306/c2096406_informatic'
