bd43c36f3de2d87c4bc945697a9b4e0ed08c2224
HecSky <hecsky@163.com>
2021-04-19 23:47:44 +0800

    export files

--- website/routes.py
+++ website/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, url_for, request, redirect, flash
+from flask import render_template, url_for, request, redirect, flash, send_from_directory
 from website import app
+from website.export.export import exportXML, exportJSON_LD
 from website.forms import IndexForm
 from website.functions import Search
 
@@ -31,8 +32,53 @@ def index():
     return render_template('index.html', form=form)
 
 
-@app.route('/result')
+@app.route('/result', methods=['GET', 'POST'])
 def result():
+    form = IndexForm()
     data = Search(value)
-    print(data)
-    return render_template('result.html', data=data)
+    dataList = []
+    for item in data:
+        tmp = [item.Phone_id,
+               item.Phone_name,
+               item.Phone_price,
+               item.Phone_factory_system_kernel,
+               item.Phone_screen_size,
+               item.Phone_OS,
+               item.Phone_resolution,
+               item.Phone_frequency,
+               item.Phone_kernel_num,
+               item.Phone_RAM_capacity,
+               item.Phone_ROM_capacity,
+               item.Phone_battery_capacity,
+               item.Phone_rear_camera,
+               item.Phone_front_camera,
+               item.Phone_pic_URL,
+               item.Phone_brand,
+               item.Phone_target_group]
+        dataList.append(tmp)
+    exportXML(dataList)
+    exportJSON_LD(dataList)
+    if request.method == 'POST':
+        print(1)
+        if 'XML' in request.form:
+            print('XML')
+            return redirect(url_for('downloadXML'))
+        elif 'JSONLD' in request.form:
+            print('JSONLD')
+            return redirect(url_for('downloadJSONLD'))
+    print(2)
+    return render_template('result.html', data=data, form=form)
+
+
+@app.route('/downloadXML', methods=['GET', 'POST'])
+def downloadXML():
+    print('WEBXML')
+    return send_from_directory(app.root_path + '/export/', 'phones.xml', as_attachment=True)
+    pass
+
+
+@app.route('/downloadJSONLD', methods=['GET', 'POST'])
+def downloadJSONLD():
+    print('WEBJSON')
+    return send_from_directory(app.root_path + '/export/', 'phones.jsonld', as_attachment=True)
+    pass
